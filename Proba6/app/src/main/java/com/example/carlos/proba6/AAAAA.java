fd385234af25238783aecceeea59991acd8aa21b
carloscarrion <carrion@edu.xunta.es>
2015-10-05 18:21:20 +0200

    a feixe

---
+++ Proba6/app/src/main/java/com/example/carlos/proba6/AAAAA.java
@@ -0,0 +1,13 @@
+package com.example.carlos.proba6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AAAAA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aaaa);
+    }
+}
